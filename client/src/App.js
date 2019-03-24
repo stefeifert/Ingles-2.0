a833cae1795dba5578bef88c94fbfe6a732742f9
Shiny <stefanie.eifert@yahoo.com>
2019-03-24 13:39:32 -0400

    updated app.js and index.js files

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import LoginPage from "./components/LoginPage";
+import HomePage from "./components/HomePage";
+import UserContext from "./context/UserContext";
 
 class App extends Component {
+
+  state = {
+    user: null
+  }
+
+  setUser = (user) => {
+	  this.setState({ user });
+  }
+
   render() {
+	const {user} = this.state;
+	const setUser = this.setUser;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+		<Router>
+			<div>
+				<header>
+					<nav>
+						<Link to="/">Home</Link> | <Link to="/login">Login</Link>
+					</nav>
+				</header>
+				<UserContext.Provider value={{ setUser, user }}>
+					<ProtectedRoute exact path="/" component={HomePage} />
+					<Route exact path="/login" component={LoginPage} />
+				</UserContext.Provider>
+			</div>
+		</Router>
     );
   }
 }
