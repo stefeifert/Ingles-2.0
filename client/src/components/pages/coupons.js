d8ab718ae21233843d03da67444c4851a00386ea
Mark <markherrington5@gmail.com>
2019-03-26 10:26:42 -0400

    coupons rendering

--- client/src/components/pages/coupons.js
+++ client/src/components/pages/coupons.js
@@ -1,8 +1,13 @@
 import React from 'react';
 // import { Link } from 'react-router-dom'
 // import './landing.css'
-import Navbar from '../navbar'
-import Sidebar from '../sidebar'
+import Navbar from '../navbar';
+import Sidebar from '../sidebar';
+import AmysCoupon from '../images/coupons/AmysCoupon.jpg';
+import bananaCoupon from '../images/coupons/bananaCoupon.jpg';
+import coffeeCoupon from '../images/coupons/coffeeCoupon.jpg';
+import dogFoodCoupon from '../images/coupons/dogFoodCoupon.jpg'
+import OreoCoupon from '../images/coupons/OreoCoupon.jpg';
 
 class Coupons extends React.Component {
     state = {}
@@ -21,6 +26,11 @@ class Coupons extends React.Component {
                     <li><a href='shopping-list'>Saved Shopping List</a></li>
                     <li><a href='about-us'>About Us</a></li>
                 </ul>
+                <img src={OreoCoupon} alt='OreoCoupon' width='650' />
+                <img src={AmysCoupon} alt='AmysCoupon' width='650' />
+                <img src={bananaCoupon} alt='bananaCoupon' width='650' />
+                <img src={coffeeCoupon} alt='coffeeCoupon' width='650' />
+                <img src={dogFoodCoupon} alt='dogFoodCoupon' width='650' />
             </div>
         )
     }
