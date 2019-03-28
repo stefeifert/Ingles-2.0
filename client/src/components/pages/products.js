31c9c86c2bd0e26f98518690ee37bd57dd83b104
ulyssesfisher <ufisher@gmail.com>
2019-03-28 14:54:53 -0400

    render products to page

--- client/src/components/pages/products.js
+++ client/src/components/pages/products.js
@@ -3,9 +3,23 @@ import React from 'react';
 // import './landing.css'
 import Navbar from '../navbar'
 import Sidebar from '../sidebar'
+import axios from 'axios'
+
 
 class Products extends React.Component {
-    state = {}
+    state = {
+        products: []
+    }
+componentDidMount(){
+axios.get("/api/products")
+.then(res => this.setState({
+    products: res.data
+}))
+
+
+}
+
+
 
     render() {
         return (
@@ -13,6 +27,10 @@ class Products extends React.Component {
                 <Navbar />
                 <Sidebar />
                 <h1>Products Page</h1>
+                {this.state.products.map(product => (
+                    <p>{product.item}</p>
+                ))}
+
             </div>
         )
     }
