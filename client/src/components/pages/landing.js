0468fabed5ed84c3fd95aaf6bdf3e276c3b4cb08
Manny Cortez <emailme@mannycortez.com>
2019-03-28 14:47:05 -0400

    working hamberger and takes out container

--- client/src/components/pages/landing.js
+++ client/src/components/pages/landing.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import Navbar from '../navbar'
-import Carousel from '../Carousel/Carousel';
-import Container from 'react-bootstrap/Container';
-import '../Carousel/carousel.css';
-import Sidebar from '../sidebar';
+import React from "react";
+import Navbar from "../navbar";
+import Carousel from "../Carousel/Carousel";
+import Container from "react-bootstrap/Container";
+import "../Carousel/carousel.css";
+import Sidebar from "../sidebar";
 
 const Landing = props => {
-    return (
-        <Container>
-        <div>
-            <Navbar />
-            <h1>Home Page</h1>
-            <Carousel />
-        </div>
-        </Container>
-    )
+  return (
+    <div>
+      {" "}
+      <Sidebar />
+      <Navbar />
+      <h1>Home Page</h1>
+      <Carousel />
+    </div>
+  );
 };
 
 export default Landing;
