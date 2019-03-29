44640df7bb61b3b037651f9bd090a81a2dec1817
ulyssesfisher <ufisher@gmail.com>
2019-03-29 08:57:45 -0400

    cards rendering to products

--- client/src/components/pages/products.js
+++ client/src/components/pages/products.js
@@ -1,31 +1,56 @@
 import React from 'react';
 // import { Link } from 'react-router-dom'
 // import './landing.css'
-import axios from 'axios'
+import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
 
 
 class Products extends React.Component {
     state = {
         products: []
     }
-componentDidMount(){
-axios.get("/api/products")
-.then(res => this.setState({
-    products: res.data
-}))
+    componentDidMount() {
+        axios.get("/api/products")
+            .then(res => this.setState({
+                products: res.data
+            }))
 
 
-}
-
-
+    }
 
     render() {
         return (
             <div className='page products'>
                 <h1>Products Page</h1>
                 {this.state.products.map(product => (
-                    <p>{product.item}</p>
+
+                    <div className="container">
+                        
+                            <Card style={{ width: '18rem', margin: '2rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>{product.item}</Card.Title>
+                                    <Card.Subtitle className="mb-2 text-muted">Advantage Buy! {product.price}</Card.Subtitle>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                       
+                    </div>
+
+
+
+
                 ))}
+
 
             </div>
         )
