2f718a45f2af342227509afc5ecbd8763569f0e5
Mark <markherrington5@gmail.com>
2019-03-29 12:04:25 -0400

    fixed 1/3 of prod-imgs, changed .container class to .carouselContainer, fixed models, added drop to seed-item, fixed bootstrap grid on products.js, fixed products.js cards

--- client/src/components/pages/products.js
+++ client/src/components/pages/products.js
@@ -1,60 +1,60 @@
-import React from 'react';
+import React from "react";
 // import { Link } from 'react-router-dom'
 // import './landing.css'
-import axios from 'axios';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-
-
+import axios from "axios";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 class Products extends React.Component {
-    state = {
-        products: []
-    }
-    componentDidMount() {
-        axios.get("/api/products")
-            .then(res => this.setState({
-                products: res.data
-            }))
+  state = {
+    products: []
+  };
+  componentDidMount() {
+    axios.get("/api/products").then(res =>
+      this.setState({
+        products: res.data
+      })
+    );
+  }
 
-
-    }
-
-    render() {
-        return (
-            <div className='page products'>
-                <h1>Products Page</h1>
-                {this.state.products.map(product => (
-
-                    <div className="container">
-                        
-                            <Card style={{ width: '18rem', margin: '2rem' }}>
-                                <Card.Img variant="top" src="holder.js/100px180" />
-                                <Card.Body>
-                                    <Card.Title>{product.item}</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted">Advantage Buy! {product.price}</Card.Subtitle>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
-                                </Card.Body>
-                            </Card>
-                       
-                    </div>
-
-
-
-
-                ))}
-
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Container className="page products">
+        <h1>Products Page</h1>
+        <Row>
+          {this.state.products.map(product => (
+            <Card key={product._id} style={{ width: "18rem", margin: "2rem" }}>
+              <Card.Img
+                variant="top"
+                height='auto'
+                width='150px'
+                src={
+                  product.filename
+                    ? require(`../images/prod-imgs/${product.filename}`)
+                    : null
+                }
+              />
+              <Card.Body>
+                <Card.Title>{product.item}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  Advantage Buy! {product.price}
+                </Card.Subtitle>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default Products;
--- client/src/components/pages/shoppingList.js
+++ client/src/components/pages/shoppingList.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React from "react";
 // import { Link } from 'react-router-dom'
 // import './landing.css'
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 class ShoppingList extends React.Component {
-    state = {}
+  state = {};
 
-    render() {
-        return (
-            <div className='page ShoppingList'>
-                <h1>Shopping List Page</h1>
+  render() {
+    return (
+      <div className="page ShoppingList">
+        <h1>Shopping List Page</h1>
+        <Container style={{ background: "green" }}>
+          <Row>
+            <Col style={{ background: "blue" }}>1 of 2</Col>
+            <Col style={{ background: "yellow" }}>2 of 2</Col>
+          </Row>
 
-
-
-                
-            </div>
-        )
-    }
+          <Row>
+            <Col>1 of 3</Col>
+            <Col>2 of 3</Col>
+            <Col>3 of 3</Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default ShoppingList;
